e8f7667752cd167a5f20c0e782535c9c3c0acf3d
HMasataka <siruba.2050@gmail.com>
2023-02-19 13:41:24 +0900

    Feat struct E

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+type E struct {
+	Code int
+	Err  error
+}
+
+func (e E) Error() string {
+	return e.Err.Error()
+}
+
+func main() {
+	fmt.Println(New())
+}
+
+func New() error {
+	return E{Code: http.StatusBadRequest, Err: errors.New("error")}
+}
